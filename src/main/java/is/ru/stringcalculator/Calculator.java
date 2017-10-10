b900196e62ea98ca0befcf2d4557f7dfa8b652b5
asgeiro14 <asgeir14@ru.is>
2017-10-10 15:10:19 +0000

    Testing two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,10 +3,16 @@ package is.ru.stringcalculator;
 public class Calculator{
 
     public static int add(String text){
+
             if(text.equals("")){
                 return 0;
             }
-            else
+            else {
+                if(text.contains(",")){
+                    String numbers[] = text.split(",");
+                    return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+                }
                 return 1;
+            }
     }
 }
